444b1bf867dfa6b7b868bd524e6aa8a6b99a8966
Albatross3 <superstring77@gmail.com>
2022-12-07 14:40:33 +0900

    feat: 관리자 페이지 UI 구현 (타임리프)

--- src/main/java/com/demo/agent/AgentApplication.java
+++ src/main/java/com/demo/agent/AgentApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AgentApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AgentApplication.class, args);
 	}
